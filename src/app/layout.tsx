1274352436a21383e1514867cd702a99ca1b2524
HectorCIT <hector.henriquez@cit.hn>
2025-07-24 11:11:12 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
           <AlertProvider>
             {children}
             <GlobalAlert />
-            {process.env.NODE_ENV === 'development' && <RecaptchaTestingPanel />}
+            <RecaptchaTestingPanel />
           </AlertProvider>
         </RecaptchaEnterpriseProvider>
       </body>
